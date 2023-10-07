3b6996fa9596a969978951d70883ca374c423dca
33-66 <simon.nganga@student.moringaschool.com>
2023-10-07 15:53:02 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+//write your code here
+const num1 = 31;
+const num2 = 2
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random())
+
+
+
+const num3 = 25
+const num4 = 7
+
+const mod = num3 % num4
+
+const max = Math.max(15, 16, 17, 18 , 19 , 20)
